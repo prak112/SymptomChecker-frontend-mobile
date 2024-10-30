6ff682d1ba08a701d748fdafd3a4d3d31825ac53
PrakGov <55034062+prak112@users.noreply.github.com>
2024-10-30 20:10:47 +0200

    feat: Add navigation setup header, footer and update styles

--- symptom-checker/App.js
+++ symptom-checker/App.js
@@ -1,29 +1,62 @@
 /**
  * Node modules
  */
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 /**
- * Components
+ * Context Providers
+ */
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+
+/**
+ * Components and Screens 
  */
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './screens/Home';
 
+
+/**
+ * Styles - App <View />
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFEB50', // Zinc yellow color
     alignItems: 'center',
-    justifyContent: 'center',
-    pointerEvents: 'box-only',
+    justifyContent: 'flex-start',
+    // pointerEvents: 'box-only',
   },
 });
 
+/**
+ * The main App component that sets up the application's structure.
+ * It includes a header, home content, and footer within a navigation container.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered component.
+ */
 export default function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+      setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <View style={ styles.container }>
-      <Header />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+      <StatusBar />
+        <View style={ styles.container }>
+          <Header toggleDrawer={toggleDrawer} />
+          <Home />
+          <Footer />
+        </View>
+      </NavigationContainer>
+    </PaperProvider>
+
   );
 }
-
