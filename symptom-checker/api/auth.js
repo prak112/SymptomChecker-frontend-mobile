c7704218f7f18242652dceb4beab6a6fae3257c9
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-07 21:21:49 +0200

    feat: Implement user auth API

---
+++ symptom-checker/api/auth.js
@@ -0,0 +1,46 @@
+// import api setup
+import { NODE_BACKEND_URL, EXPRESS_API_AUTH_URL, client } from "./utils/setup";
+
+
+// POST - Guest User registration and login
+export const createGuestUser = async() => {
+    try {
+        const response = await client.post(`${EXPRESS_API_AUTH_URL}/guest`)
+        return response.data
+    } catch (error) {
+        console.error('Error during user registration : ', error)
+        throw error;
+    }
+}
+
+// POST - User registration
+export const registerUser = async(userInformation) => {
+    try {
+        const response = await client.post(`${EXPRESS_API_AUTH_URL}/signup`, userInformation)
+        return response.data
+    } catch (error) {
+        console.error('Error during user registration : ', error)
+        throw error;
+    }
+}
+
+// POST - User login
+export const authenticateUser = async(userInformation) => {
+    try {
+        const response = await client.post(`${EXPRESS_API_AUTH_URL}/signup`, userInformation)
+        return response.data
+    } catch (error) {
+        console.error('Error during user registration : ', error)
+        throw error;
+    }
+}
+
+// POST - User Logout
+export const invalidateUserSession = async() => {
+    try {
+        await client.post(`${EXPRESS_API_AUTH_URL}/logout`)
+    } catch (error) {
+        console.error('Error during user registration : ', error)
+        throw error;
+    }
+}
--- symptom-checker/api/symptoms.js
+++ symptom-checker/api/symptoms.js
@@ -1,23 +1,11 @@
-import axios from "axios"
-
-// import backend config from .env
-const NODE_BACKEND_URL = process.env.EXPO_PUBLIC_NODE_BACKEND_URL
-const EXPRESS_API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL
-
-// Setup client config
-const client = axios.create({
-  baseURL: NODE_BACKEND_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-const apiURL = EXPRESS_API_BASE_URL
+// import api setup
+import { NODE_BACKEND_URL, EXPRESS_API_SERVICE_URL, client } from "./utils/setup";
 
 
 // POST - 'General' search result from symptoms payload (symptoms + diagnosis preference)
 export const getGeneralDiagnosis = async(symptoms) => {
     try {
-        const response = await client.post(`${apiURL}/symptoms/general`, symptoms)
+        const response = await client.post(`${EXPRESS_API_SERVICE_URL}/symptoms/general`, symptoms)
         console.log('client service calling backend for General Diagnosis...')
         return response.data
     } catch (error) {
@@ -29,7 +17,7 @@ export const getGeneralDiagnosis = async(symptoms) => {
 // POST - 'Specific' search result from symptoms payload (symptoms + diagnosis preference)
 export const getSpecificDiagnosis = async(symptoms) => {
     try {
-        const response = await client.post(`${apiURL}/symptoms/specific`, symptoms)
+        const response = await client.post(`${EXPRESS_API_SERVICE_URL}/symptoms/specific`, symptoms)
         console.log('client service calling backend for Specific Diagnosis...')
         return response.data
     } catch (error) {
