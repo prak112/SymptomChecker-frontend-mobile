e08166a09ae7f8423a7ef4c2beb79c7fd60b41b8
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-07 21:25:09 +0200

    feat: Add authentication modal

--- symptom-checker/screens/Home.jsx
+++ symptom-checker/screens/Home.jsx
@@ -1,12 +1,41 @@
 /**
  * Node modules
  */
+import React, {useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 
 /**
  * Screens
  */
 import SymptomForm from "./SymptomForm";
+import AuthenticationModal from "./auth/AuthenticationModal";
+
+
+/**
+ * Home component renders the main screen of the application.
+ * It includes a button to show the authentication modal and symptom form.
+ */
+export default function Home() {
+    const [showAuth, setShowAuth] = useState(true)
+    const handleHideModal = () => setShowAuth(false)
+    
+    return (
+        <View style={styles.container}>
+            <Button
+                onPress={() => setShowAuth(true)}
+            >
+                Login / Sign Up
+            </Button>
+            <AuthenticationModal 
+                visible={showAuth}
+                hideModal={handleHideModal}
+            />
+            <SymptomForm />
+        </View>
+    );
+}
+  
 
 /**
  * Styles
@@ -18,15 +47,3 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
     }
 })
-
-/**
- * Home screen renders the main view for the home screen.
- */
-export default function Home() {
-    return (
-        <View style={styles.container}>
-            <SymptomForm />
-        </View>
-    );
-  }
-  
