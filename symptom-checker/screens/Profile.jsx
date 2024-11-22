d0344c45400712ef7bb7ededb864e94a18dd52f7
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-22 21:11:32 +0200

    feat: Add conditional navigation and button for login/logout

--- symptom-checker/screens/Profile.jsx
+++ symptom-checker/screens/Profile.jsx
@@ -8,6 +8,8 @@ import { useContext } from 'react';
  * Context
  */
 import { UserContext } from '@/contexts/UserContext';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 
 /**
@@ -15,6 +17,16 @@ import { UserContext } from '@/contexts/UserContext';
  */
 export default function Profile() {
     const { user } = useContext(UserContext)
+    const navigation = useNavigation()
+
+    const handleRedirect = () => {
+        if (user && user.username) {
+            navigation.navigate('Logout')
+        } else {
+            navigation.navigate('Login')
+        }
+    }
+
     return(
         <View style={styles.container}>
             <Text>
@@ -22,6 +34,14 @@ export default function Profile() {
                 Username : { user ? user.username : 'Not logged in' } <br/>
                 Joined on : { user ? user.registrationTime : 'Not logged in' }
             </Text>
+           <Button 
+                mode="contained" 
+                onPress={handleRedirect}
+                style={user ? styles.logoutButton : styles.loginButton}
+                icon={user ? "logout" : "login"}
+            >
+                {user ? 'Logout' : 'Login'}
+            </Button>
         </View>
     )
 }
@@ -30,9 +50,16 @@ export default function Profile() {
  * Styles
  */
 const styles = StyleSheet.create({
-    container: { 
+    container: {
+        backgroundColor: '#beed', 
         flex: 1, 
         justifyContent: 'space-around',
         alignItems: 'center', 
-    }
+    },
+    loginButton: {
+        marginTop: 16,
+    },
+    logoutButton: {
+        backgroundColor: '#d32f2d',
+    },
 })
