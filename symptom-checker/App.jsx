b83b922e50e0fd50425ba3248518fcbb9c6cd840
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-22 21:19:22 +0200

    feat: Implement authentication and logout modals in navigation

--- symptom-checker/App.jsx
+++ symptom-checker/App.jsx
@@ -3,16 +3,19 @@
  */
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { useState, useContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /**
  * Components
  */
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ErrorBoundary from './components/ErrorBoundary';
 
 /**
  * Screens
@@ -21,54 +24,118 @@ import Home from './screens/Home';
 import Profile from './screens/Profile';
 import History from './screens/History';
 import FAQs from './screens/FAQs';
+import LogoutModal from './screens/auth/LogoutModal';
+import AuthenticationModal from './screens/auth/AuthenticationModal';
+
+/**
+ * Context
+ */
+import { UserContext, UserProvider } from './contexts/UserContext';
+import { invalidateUserSession } from './api/auth';
 
 
 // Create navigators
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+// Modal Wrapper
+function ModalWrapper({ route }) {
+  const navigation = useNavigation()
+  const [showAuth, setShowAuth] = useState(true)
+  const [showLogout, setShowLogout] = useState(true)
+
+  // toggle modal visibility
+  useEffect(() => {
+    switch (route.name) {
+      case 'Login':
+        setShowAuth(true);
+        setShowLogout(false);
+        break;
+      case 'Logout':
+        setShowLogout(true);
+        setShowAuth(false);
+        break;
+      case 'Home':
+      default:
+        setShowLogout(false);
+        setShowAuth(false);
+        break;
+    }
+  }, [route.name]);
+
+  const handleAuthModal = () => {
+    setShowAuth(false);
+    navigation.navigate('Home');
+    console.log('AuthModal Visible status : ', showAuth)
+  };
+
+  const handleLogoutModal = () => {
+    setShowLogout(false);
+    navigation.navigate('Home');
+    console.log('LogoutModal Visible status : ', showLogout)
+  };
+
+
+  if(route.name === 'Login') {
+    return (
+      <AuthenticationModal
+        visible={showAuth}
+        hideModal={handleAuthModal}
+      />
+    )
+  } else if(route.name === 'Logout') {
+    return (
+      <LogoutModal
+        visible={showLogout}
+        hideModal={handleLogoutModal}
+      />
+    )
+  }
+
+  return null;
+}
+
+
 // Drawer Navigator Component
-function DrawerNavigator() {
+function DrawerNavigator({ modalVisible, handleModal }) {
+  const { user } = useContext(UserContext);
+
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        header: (props) => <Header {...props} />,
-        drawerStyle: {
-          backgroundColor: '#fff',
-          width: 280,
-        },
-      }}
-    >
-      <Drawer.Screen 
-        name="HomeStack" 
-        component={HomeStackNavigator}
-        options={{ 
-          title: 'Home',
-          drawerLabel: 'Home'
-        }} 
-      />
-      <Drawer.Screen 
-        name="Profile" 
-        component={Profile}
-        options={{ 
-          drawerLabel: 'User Profile'
-        }} 
-      />
-      <Drawer.Screen 
-        name="History" 
-        component={History}
-        options={{ 
-          drawerLabel: 'Search History'
-        }} 
-      />
-      <Drawer.Screen 
-        name="FAQs" 
-        component={FAQs}
-        options={{ 
-          drawerLabel: 'FAQs'
-        }} 
-      />
-    </Drawer.Navigator>
+    // <UserProvider>
+      <Drawer.Navigator
+        screenOptions={{
+          header: () => <Header />,
+          drawerStyle: {
+            backgroundColor: '#bcd',
+            width: 280,
+          },
+        }}
+      >
+        <Drawer.Screen 
+          name="HomeStack" 
+          component={HomeStackNavigator}
+          options={{ 
+            title: 'Home',
+            drawerLabel: 'Home'
+          }} 
+        />
+        <Drawer.Screen name="Profile" component={Profile} />
+        <Drawer.Screen 
+          name="History" 
+          component={History}
+          options={{ 
+            drawerLabel: 'Search History'
+          }} 
+        />
+        <Drawer.Screen name="FAQs" component={FAQs} />
+        {/* Auth screens */}
+        {!user ? (
+          <Drawer.Screen name="Login" component={ModalWrapper} />
+        ) : (
+          <Drawer.Screen name="Logout" component={ModalWrapper} />
+        )}
+      </Drawer.Navigator>
+    // </UserProvider>
   );
 }
 
@@ -77,46 +144,84 @@ function HomeStackNavigator() {
   return (
     <Stack.Navigator
       screenOptions={{
-        header: (props) => <Header {...props} />,
+        header: () => <Header />,
       }}
     >
       <Stack.Screen 
         name="Home" 
         component={Home}
         options={{ 
-          headerShown: false, // Hide header for Home screen as Drawer header will be shown
+          headerShown: false, // Hide header for Home screen as Drawer Navigator is opened
           cardStyle: {
             backgroundColor: '#beeced',
           }
         }} 
       />
-      {/* Stack screens to add here */}
+      <Stack.Screen 
+        name="Login" 
+        component={ModalWrapper} 
+        options={{ 
+          headerShown: false, // Hide header for Home screen as Drawer Navigator is opened
+          cardStyle: {
+            backgroundColor: '#beeced',
+          }
+        }}
+      />
+      {/* Stack screens to add here */}      
     </Stack.Navigator>
   );
 }
 
 // Main App Component
 export default function App() {
+  const { user, setUser } = useContext(UserContext);
+
+  // retrieve authenticated user
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await AsyncStorage.getItem('authenticatedUser')
+        if (user) {
+          console.log('User context value set. Username : ', user.username)
+          setUser(user)
+        }
+      } catch (error) {
+        console.error('Error fetching user data : ', error)
+        setUser(null)
+      }
+    }
+    fetchUser()
+  }, []);
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <PaperProvider>
-        <NavigationContainer>
-          <StatusBar style="auto" />
-          <DrawerNavigator />
-          <Footer />
-        </NavigationContainer>
-      </PaperProvider>
+        <PaperProvider>
+          <ErrorBoundary>
+            <UserProvider>
+              <NavigationContainer>
+                <View style={styles.content}>
+                  <StatusBar style="auto" />
+                  <DrawerNavigator />
+                </View>
+                <Footer />
+              </NavigationContainer>
+            </UserProvider>
+          </ErrorBoundary>
+        </PaperProvider>
     </SafeAreaView>
-
   );
 }
 
 /**
- * Styles for rendering the app
+ * Styles for App
  */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    overflow: 'hidden',
   },
+  content: {
+    flex: 1,
+    paddingBottom: 200, // prevent content from being hidden behind footer
+  }
 });
